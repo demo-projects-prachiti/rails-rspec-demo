ccb20bb10ce798620f14131385af4e4bae1982a7
Prachiti Mhatre <prachiti.s.mhatre@gmail.com>
2022-05-13 19:02:17 +0530

    changed mail configuration settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "blog_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {host: 'https://neoblogweb.herokuapp.com/'}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+ ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['GMAIL_USERNAME'],
+  :password => ENV['GMAIL_PASSWORD'],
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
