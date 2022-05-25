d6a150e980c86708a33815d2ac6e0940410319dd
Prachiti Mhatre <prachiti.s.mhatre@gmail.com>
2022-05-25 17:25:11 +0530

    jquery chnages

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,8 +4,6 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
   $: 'jquery/src/jquery',
   jQuery: 'jquery/src/jquery',
-  jquery: 'jquery',
-  'window.jQuery': 'jquery',
   Popper: ['popper.js', 'default']
 }))
 
