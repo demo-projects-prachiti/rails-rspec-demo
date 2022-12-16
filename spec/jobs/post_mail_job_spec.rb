8dbb4fe5ac25c39af18604d3e46001c634583ed0
prachiti-neosoft <prachiti.mhatre@neosoftmail.com>
2022-12-16 18:10:12 +0530

    Added job for blog application

--- spec/jobs/post_mail_job_spec.rb
+++ spec/jobs/post_mail_job_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 RSpec.describe PostMailJob, type: :job do
   describe "#perform_later" do
-    it "Create a Post" do
-      ActiveJob::Base.queue_adapter = :test
+    it 'after ' do 
       post = create(:post)
-      expect {
-        PostMailJob.perform_now(post)
-      }.to have_enqueued_job
+      expect(PostMailer).to receive(:post_creation_mail)
+      PostMailer.post_creation_mail(post)
     end
   end
 end
